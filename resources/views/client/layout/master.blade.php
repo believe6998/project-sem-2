d30563e6900d81689abcffa75d4ebbe575298d61
ngoccp1234 <ngoclath1807071@fpt.edu.vn>
2019-08-28 01:14:41 +0700

    up lai

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -15,86 +15,174 @@
     <title>Document</title>
 </head>
 <body>
-<div class="container-fluid">
-<div class="row layout-header">
-    <div class="col-4 ">
+<div class="row  header-style mr-5">
+    <div class="col-2 col-md-3 col-sm-4 logo-header mt-2 mb-2 ">
         <img src="https://my-pt.online/wp-content/uploads/2017/11/alleen-mypt-text-2.png" alt="..."
              class="img-thumbnail logo-image">
     </div>
-    <div class="col-6 layout-style">
+    <div class="col-8 col-md-7 col-sm-7 navbar-header">
         <nav class="navbar navbar-expand-lg navbar-light bg-light layout-nav-bar ">
-            {{--                <a class="navbar-brand" href="#">Home</a>--}}
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="navbar-brand nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="navbar-brand nav-link active" href="#">Online Personal Training <span class="sr-only">(current)</span></a>
-                    <a class="navbar-brand nav-link active" href="#">Blog <span class="sr-only">(current)</span></a>
-                    <a class="navbar-brand nav-link active" href="#">Contact <span class="sr-only">(current)</span></a>
+                <div class="navbar-nav dropdown link-header ml-2 text-title-header">
+                    <a class="navbar-brand nav-link " href="#">Trang chủ </a>
+                    <a class="navbar-brand nav-link " href="#">Tập luyện trực tuyến</a>
+                    <a class="navbar-brand nav-link " href="#">Blog</a>
+                    <a class="navbar-brand nav-link " href="#">Liên hệ</a>
+
                 </div>
             </div>
         </nav>
     </div>
-    <div class="col-2">
-        <div class="mt-3 ml-5 dropdown  ">
-            <i class="fas fa-user  dropdown-toggle " id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ></i>
-            <div class="dropdown-menu master-dropdown-menu " aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Login</a>
-                <a class="dropdown-item" href="#">Register</a>
+    <div class="col-2 col-md-2 col-sm-1  d-flex justify-content-end icon-user-header">
+        <a href="#"><i class="fas fa-user fa dropdown-toggle"></i></a>
+    </div>
 
-            </div>
-        </div>
-    </div>
-    </div>
-    <div>
+
+</div>
+<div>
     @section('content')
     @show()
 </div>
-<div class="row footer-control">
-    <div class="col-3 footer-detail">
-        <h2 class="mt-4">LINKS</h2>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Online Training</a></li>
-            <li><a href="#">Contact-us</a></li>
-        </ul>
+<footer class="page-footer font-small unique-color-dark footer-layout">
+
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left ">
+
+        <!-- Grid row -->
+        <div class="row mt-1">
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                <!-- Content -->
+                <h6 class="text-uppercase font-weight-bold">My pt online</h6>
+                <hr class="deep-purple accent-2 mb-2 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>Chúng tôi luôn mang lại sự hài lòng và những bài hướng dẫn tốt nhất cho các bạn với dịch vụ tập luyện trực tuyến mới mẻ và bổ ích của My PT Online.</p>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 text-footer-control">
+
+                <!-- Links -->
+                <h6 class="text-uppercase font-weight-bold">liên kết</h6>
+                <hr class="deep-purple accent-2 mb-2 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a href="#">Trang chủ</a>
+                </p>
+                <p>
+                    <a href="#">Gói tập</a>
+                </p>
+                <p>
+                    <a href="#">Blog</a>
+                </p>
+                <p>
+                    <a href="#">Liên hệ</a>
+                </p>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 text-footer-control">
+
+                <!-- Links -->
+                <h6 class="text-uppercase font-weight-bold">Dịch vụ</h6>
+                <hr class="deep-purple accent-2 mb-2 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <a href="#">Điều khoản</a>
+                </p>
+                <p>
+                    <a href="#">Cookies</a>
+                </p>
+                <p>
+                    <a href="#">Gửi phản hồi</a>
+                </p>
+                <p>
+                    <a href="#">Trợ giúp</a>
+                </p>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+
+                <!-- Links -->
+                <h6 class="text-uppercase font-weight-bold">Liên hệ</h6>
+                <hr class="deep-purple accent-2 mb-2 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>
+                    <i class="fas fa-home mr-3"></i> Số 8 Tôn Thất Thuyết</p>
+                <p>
+                    <i class="fas fa-envelope mr-3 mt-3"></i> mypt@gmail.com</p>
+                <p>
+                    <i class="fas fa-phone mr-3 mt-3"></i> + 0987654321</p>
+
+            </div>
+        </div>
+
     </div>
-    <div class="col-3 footer-detail">
-        <h2 class="mt-4 ">BLOG</h2>
-        <ul>
-            <li>is simply dummy</li>
-            <li>is simply dummy</li>
-            <li>is simply dummy</li>
-            <li>is simply dummy</li>
-        </ul>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <!-- Footer -->
+
+
+    <div class="social-icon d-flex justify-content-center">
+
+        <!-- Grid row-->
+        <div class="row ">
+
+            <!-- Grid column -->
+            <div class="col-md-12  py-5 social-icon-layout" >
+                <div class=" flex-center">
+
+                    <!-- Facebook -->
+                    <a class="fb-ic" href="#">
+                        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!-- Twitter -->
+                    <a class="tw-ic"href="#" >
+                        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!-- Google +-->
+                    <a class="gplus-ic"href="#">
+                        <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!--Linkedin -->
+                    <a class="li-ic"href="#">
+                        <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!--Instagram-->
+                    <a class="ins-ic" href="#">
+                        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <!--Pinterest-->
+                    <a class="pin-ic" href="#">
+                        <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+                    </a>
+                </div>
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
     </div>
-    <div class="col-3 footer-detail">
-        <h2 class="mt-4">CONTACT</h2>
-        <ul>
-            <li><a href="#"><i class="fas fa-paper-plane mr-1"><span class="ml-2">myptonline@gmail.com</span></i></a></li>
-            <li><a href="#"><i class="fa fa-phone mt-2 mr-1"> <span class="ml-1">+0912345678</span></i></a></li>
-            <li><a href="#"><i class="fas fa-map-marker-alt mt-2 "><span class="ml-2">Số 8 Tôn Thất Thuyết</span></i></a></li>
-        </ul>
+
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+        <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
     </div>
-    <div class="col-3  footer-detail">
-        <h2 class="mt-4">SERVICES</h2>
-        <ul>
-            <li><a href="#">Algemene voorwaarden</a></li>
-            <li><a href="#">Cookies</a></li>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Instagram</a></li>
-        </ul>
-    </div>
-    <div class="row copyright-footer">
-        <div class="col-3"></div>
-        <div class="col-6 copyright-footer-text mt-lg-5 "> <p>Copyright MY-PT.Online 2018  | Ontworpen door inDSGN
-                Algemene voorwaarden | Cookies</p></div>
-        <div class="col-3 "></div>
-    </div>
-</div>
-</div>
+    <!-- Copyright -->
+
+</footer>
+
+
 </body>
 </html>
