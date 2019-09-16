379e35b25c665b8d5900ebb728c4562aa3bca32a
ADMIN <phongltth1807075@fpt.edu.vn>
2019-09-16 15:37:57 +0700

    sua lai update anh len cloudinary

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::get('/pt/detail', function () {
 });
 Route::resource('admin/blog','BlogController');
 
-Route::resource('admin/pt','AdminPTController');
+Route::resource('/admin/pt','AdminPTController');
 
 Route::resource('/blog','ClientBlogController',['as' => 'client']);
 
