763a27fe4dd363018227d6b09292455a1ed1bd21
nam <namphth2807043@fpt.edu.vn>
2019-09-24 21:37:06 +0700

    fix

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 
 
 class AdminOrderController extends Controller
@@ -14,11 +15,19 @@ class AdminOrderController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $orders = Order::whereNotIn('status', [-1])->paginate(10);
-        $data = ['orders' => $orders];
-        return view('admin.order.list-order', $data);
+        $personal_training_id = Input::get('personal_training_id');
+        $orders = DB::table('orders')
+            ->whereNotIn('status', [-1]);
+        if ($personal_training_id != null) {
+            $orders = $orders->where('personal_training_id',$personal_training_id);
+        } else {
+            $personal_training_id = null;
+        }
+        $orders = $orders->paginate(10);
+        $currentPersonalTrainingID = $request->get('personal_training_id');
+        return view('admin.order.list-order', compact('orders','currentPersonalTrainingID'));
     }
 
     public function index2()
